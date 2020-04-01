8c6f9236743b8efc1f4092e93e1fd13a30a5ae23
Oliver Herrmann <oliver@monoxane.io>
2020-04-01 20:48:24 +1100

    Device Modification UI

--- src/ui/views/ControlPanel/Devices/components/Device.jsx
+++ src/ui/views/ControlPanel/Devices/components/Device.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Button } from 'carbon-components-react'
+import { Button, TextInput, Toggle } from 'carbon-components-react'
 import { useMutation } from 'urql'
 
 const deleteDeviceGQL = `
@@ -13,26 +13,91 @@ const Device = (props) => {
   // eslint-disable-next-line no-unused-vars
   const [deleteDeviceMutationResult, deleteDeviceMutation] = useMutation(deleteDeviceGQL)
   const device = props.devices.find((item) => { return item.id === props.deviceID })
-  return (
-    <>
-      {device.configuration && device.configuration.map((item, index) => {
-        return (
-          JSON.stringify(item)
-        )
-      })}
-      <br/>
-      { device.id !== '0' &&
-        <>
-          <Button onClick={() => deleteDeviceMutation({ idToDelete: device.id })} style={{ minWidth: '20%' }} size='small' kind="danger">
-            Delete
-          </Button>
-          <Button onClick={() => { }} style={{ minWidth: '20%' }} size='small' kind="primary">
-            Update
-          </Button>
-        </>
-      }
-    </>
-  )
+  if (device.id === '0') {
+    return (
+      <>Internal Device Has No Configuration Options</>
+    )
+  } else {
+    return (
+      <div className="bx--col-lg-10">
+        <div className="bx--grid">
+          <h4>Details</h4>
+          <div className="bx--row">
+            <div className="bx--text-input__field-wrapper bx--col">
+              <TextInput
+                type='text'
+                id='newDeviceName'
+                placeholder='Required'
+                labelText='Device Name'
+                value={device.name}
+                onClick={() => {}}
+                onChange={(e) => { }}
+              />
+            </div>
+            <div className="bx--text-input__field-wrapper bx--col">
+              <TextInput
+                type='text'
+                id='newDeviceLocation'
+                placeholder='Optional'
+                labelText='Device Location'
+                value={device.location}
+                onClick={() => {}}
+                onChange={(e) => { }}
+              />
+            </div>
+          </div><br/>
+          <div className="bx--row">
+            <div className="bx--text-input__field-wrapper bx--col bx--col-lg-4">
+              <TextInput
+                type='text'
+                id='newDeviceDescription'
+                placeholder='Optional'
+                labelText='Device Description'
+                value={device.description}
+                onClick={() => {}}
+                onChange={(e) => { }}
+              />
+            </div>
+          </div><br/>
+          <h4>Configuration</h4>
+          <div className="bx--row">
+            <p>Device Status:&nbsp;</p>
+            <Toggle
+              labelA='Disabled'
+              labelB='Enabled'
+              toggled={device.enabled}
+              onChange={() => {}}
+              onToggle={() => {}}
+              id="enableDevice"
+            />
+          </div>
+          { device.configuration && device.configuration.map((item) =>
+            <div key={item.id} className='bx-row'>
+              <div className="bx--text-input__field-wrapper">
+                <TextInput
+                  type='text'
+                  id={`configuration${item.id}`}
+                  placeholder='Optional'
+                  labelText={item.id}
+                  value={item.value}
+                  onClick={() => {}}
+                  onChange={(e) => { } }
+                />
+              </div><br/>
+            </div>
+          )}
+          <div className="bx--row">
+            <Button onClick={() => deleteDeviceMutation({ idToDelete: device.id })} style={{ minWidth: '20%' }} size='small' kind="danger">
+              Delete
+            </Button>
+            <Button onClick={() => { }} style={{ minWidth: '20%' }} size='small' kind="primary">
+              Update
+            </Button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 Device.propTypes = {
