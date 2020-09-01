95199ea8c30aac4b9d2ae21249b82a8729a00147
Oliver Herrmann <oliver@monoxane.io>
2020-09-01 21:05:33 +1000

    chore(root): Production Deployment & Docker

--- packages/ui/webpack.config.js
+++ packages/ui/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   ],
 
   output: {
-    path: path.resolve(__dirname, 'dist/ui/'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/dist/',
     filename: 'bundle.js'
   },
