d3bb6b9632ed0ddcdfc54f7ad81bb0b14d78475f
Oliver Herrmann <oliver@monoxane.io>
2020-09-15 21:54:53 +1000

    feat(core): DB init tool

--- packages/core/src/init.js
+++ packages/core/src/init.js
@@ -5,40 +5,35 @@ require('dotenv').config()
 
 initDB()
   .then(() => {
-    devices.updateOne(
-      { id: '0' },
+    return cores.insertOne(
       {
-        $set: {
-          core: process.env.DIRECTOR_CORE_ID,
-          realm: 'root',
-          label: 'BorealSystems Director',
-          location: 'The Void',
-          provider: { id: 'BorealSystems-DirectorInternal', label: 'BorealDirector' },
-          enabled: true,
-          status: STATUS.UNKNOWN,
-          description: 'The Director Core'
-        }
-      },
-      { upsert: true }
+        id: process.env.DIRECTOR_CORE_ID,
+        label: process.env.DIRECTOR_CORE_LABEL,
+        systemNotes: 'Welcome to Director!\n\nAdministrators can edit this note via the Core Configuration page.\n\nYou should probably include some useful notes here, like who manages and administers this system, and a link to your internal helpdesk or ticketing system for users having problems.',
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        realms: [
+          {
+            id: 'root',
+            label: 'Default',
+            description: `The Default Realm on ${process.env.DIRECTOR_CORE_LABEL}`
+          }
+        ]
+      }
     )
   })
   .then(() => {
-    cores.updateOne(
-      { id: process.env.DIRECTOR_CORE_ID },
+    return devices.insertOne(
       {
-        $set: {
-          id: process.env.DIRECTOR_CORE_ID,
-          label: process.env.DIRECTOR_CORE_LABEL,
-          realms: [
-            {
-              id: 'root',
-              label: 'Default',
-              description: `The Default Realm on ${process.env.DIRECTOR_CORE_LABEL}`
-            }
-          ]
-        }
-      },
-      { upsert: true }
+        core: process.env.DIRECTOR_CORE_ID,
+        realm: 'root',
+        id: '0',
+        label: 'BorealSystems Director',
+        location: 'The Void',
+        provider: { id: 'BorealSystems-DirectorInternal', label: 'BorealDirector' },
+        enabled: true,
+        status: STATUS.UNKNOWN,
+        description: 'The Director Core'
+      }
     )
   })
   .then(() => {
