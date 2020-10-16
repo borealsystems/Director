92585937b479a1a2aa57a50516c584d250ecce8f
Oliver Herrmann <oliver@monoxane.io>
2020-10-16 23:02:07 +1100

    chore(library): Update

--- packages/core/src/utils/statusEnum.js
+++ packages/core/src/utils/statusEnum.js
@@ -1,9 +1,9 @@
 const STATUS = {
-  UNKNOWN: -1,
-  CLOSED: 0,
-  OK: 1,
-  CONNECTING: 2,
-  ERROR: 3
+  UNKNOWN: 'UNKNOWN',
+  CLOSED: 'Disconnected',
+  OK: 'OK',
+  CONNECTING: 'Connecting',
+  ERROR: 'Error'
 }
 
 export default STATUS
