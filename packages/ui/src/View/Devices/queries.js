a54aaa7911d8276629ffed93190bee873e6e961c
Oliver Herrmann <oliver@monoxane.io>
2020-11-02 15:56:33 +1100

    reactor(ui): device files

--- packages/ui/src/View/Devices/queries.js
+++ packages/ui/src/View/Devices/queries.js
@@ -23,4 +23,55 @@ mutation deleteDevice($idToDelete: String!) {
 }
 `
 
-export { devicesQueryGQL, deleteDeviceGQL }
+const newDeviceQuery = `
+query providers {
+  providers {
+    id
+    label
+    parameters {
+      required
+      id
+      label
+      regex
+    }
+  }
+}
+`
+
+const existingDeviceQuery = `
+query deviceAndProviders($id: String) {
+  providers {
+    id
+    label
+    parameters {
+      required
+      id
+      label
+      regex
+    }
+  }
+  device(id:$id){
+    id
+    label
+    location
+    description
+    status 
+    configuration {
+      id
+      value
+    }
+    provider {
+      id
+      label
+    }
+  }
+}
+`
+
+const deviceMutationGQL = `mutation device($device: deviceUpdate) {
+  device(device: $device) {
+    id
+  }
+}`
+
+export { devicesQueryGQL, deleteDeviceGQL, newDeviceQuery, existingDeviceQuery, deviceMutationGQL }
