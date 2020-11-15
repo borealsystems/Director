4bbd91c823f55d8a292d59e71986611d59ca2b52
Oliver Herrmann <oliver@monoxane.io>
2020-11-16 00:25:29 +1100

    feat(core): move init script to startup

--- packages/core/src/main.js
+++ packages/core/src/main.js
@@ -2,7 +2,8 @@ import { initDevices, cleanupDevices } from './devices'
 import { initProviders } from './providers'
 import { initApollo } from './network/apollo'
 import { initBridges } from './bridges'
-import { initDB } from './db'
+import { initDB, cores, devices } from './db'
+import STATUS from './utils/statusEnum'
 import log from './utils/log'
 import fs from 'fs'
 
@@ -11,10 +12,46 @@ require('dotenv').config()
 initApollo()
 initBridges()
 initDB()
+  .then(() => {
+    cores.countDocuments({}, (err, count) => {
+      if (err) log('error', 'core', err)
+      if (count === 0) {
+        log('info', 'core', 'Initialising Database')
+        cores.insertOne(
+          {
+            id: process.env.DIRECTOR_CORE_ID,
+            label: process.env.DIRECTOR_CORE_LABEL,
+            timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+            helpdeskVisable: false,
+            realms: [
+              {
+                id: 'ROOT',
+                label: 'Root',
+                description: `The Root Realm on ${process.env.DIRECTOR_CORE_LABEL}`,
+                notes: 'Welcome to Director!\n\nAdministrators can edit this note via the Realm Configuration page.\n\nYou should probably include some useful notes here, like who manages and administers this system, and a link to your internal helpdesk or ticketing system for users having problems.'
+              }
+            ]
+          }
+        )
+        devices.insertOne(
+          {
+            core: process.env.DIRECTOR_CORE_ID,
+            realm: 'ROOT',
+            id: `CORE-${process.env.DIRECTOR_CORE_ID}`,
+            label: process.env.DIRECTOR_CORE_LABEL,
+            location: 'The Void',
+            provider: { id: 'ProtocolProviderBorealDirector', label: 'BorealDirector' },
+            enabled: true,
+            status: STATUS.UNKNOWN,
+            description: `The virtual device for Core ${process.env.DIRECTOR_CORE_ID}`
+          }
+        )
+      }
+    })
+  })
   .then(() => initProviders())
   .then(() => initDevices())
-  .catch(e => log('error', 'core', e)
-  )
+  .catch(e => log('error', 'core', e))
 
 process.on('SIGINT', () => {
   fs.appendFileSync('logs.txt', '========= TERMINATING =========\r\n')
