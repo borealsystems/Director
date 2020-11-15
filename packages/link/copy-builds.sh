28e5f599b959ee61289c7aeb5f599a5997792fd7
Oliver Herrmann <oliver@monoxane.io>
2020-11-15 16:48:26 +1100

    fix(link): build scripts

--- packages/link/copy-builds.sh
+++ packages/link/copy-builds.sh
@@ -5,7 +5,6 @@ mv dist/link-macos dist/macos/
 mv dist/link-linux dist/linux/
 mv dist/link-win.exe dist/windows/
 mv dist/xdg-open dist/linux/
-mv dist/fsevents.node dist/macos/
 zip -r dist/macos.zip dist/macos
 zip -r dist/linux.zip dist/linux
 zip -r dist/windows.zip dist/windows
--- packages/link/copy-deps.sh
+++ packages/link/copy-deps.sh
@@ -1,2 +1,1 @@
 cp -r ../../node_modules/open/xdg-open dist/
-cp ../../node_modules/watchpack/node_modules/fsevents/fsevents.node dist/
