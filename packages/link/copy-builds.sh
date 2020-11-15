a0d5685609924f295a04bd4d09a681e55f6408c2
Oliver Herrmann <oliver@monoxane.io>
2020-11-15 20:35:27 +1100

    fix(link): build scripts

--- packages/link/copy-builds.sh
+++ packages/link/copy-builds.sh
@@ -5,6 +5,9 @@ mv dist/link-macos dist/macos/
 mv dist/link-linux dist/linux/
 mv dist/link-win.exe dist/windows/
 mv dist/xdg-open dist/linux/
+cp dist/HID.node dist/linux/
+cp dist/HID.node dist/macos/
+cp dist/HID.node dist/windows/
 zip -r dist/macos.zip dist/macos
 zip -r dist/linux.zip dist/linux
 zip -r dist/windows.zip dist/windows
--- packages/link/copy-deps.sh
+++ packages/link/copy-deps.sh
@@ -1,1 +1,2 @@
 cp -r ../../node_modules/open/xdg-open dist/
+cp -r ../../node_modules/node-hid/build/Release/HID.node dist/
