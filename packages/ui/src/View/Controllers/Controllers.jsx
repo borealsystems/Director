1d30e48b0e99e40c0eb9d2c731b6dc6042c090a3
Oliver Herrmann <oliver@monoxane.io>
2020-11-17 15:14:12 +1100

    feat(ui): empty states

--- packages/ui/src/View/Controllers/Controllers.jsx
+++ packages/ui/src/View/Controllers/Controllers.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react'
 import { useQuery, useMutation } from 'urql'
-import { DataTable, DataTableSkeleton, Pagination, Button, OverflowMenu, OverflowMenuItem } from 'carbon-components-react'
+import { DataTable, DataTableSkeleton, Pagination, Button, OverflowMenu, OverflowMenuItem, Grid, Row, Column } from 'carbon-components-react'
 import { Add24 } from '@carbon/icons-react'
 import { useHistory } from 'react-router-dom'
 import { controllersQueryGQL, deleteControllerMutationGQL } from './queries'
@@ -9,6 +9,8 @@ import DeleteObjectModal from '../components/DeleteObjectModal.jsx'
 import globalContext from '../../globalContext'
 import GraphQLError from '../components/GraphQLError.jsx'
 import headers from './headers'
+
+import image from './undraw_apps_m7mh.svg'
 
 const { Table, TableContainer, TableHead, TableHeader, TableRow, TableBody, TableCell, TableToolbar, TableToolbarContent, TableToolbarSearch } = DataTable
 
@@ -50,97 +52,130 @@ const Controllers = () => {
         .slice(begin, begin + pageSize)
       )[page - 1]
 
-    return (
-      <DataTable
-        rows={currentTableData.map(controller => ({ panelLabel: controller.panel.label, ...controller })) ?? []}
-        headers={headers}
-        render={({
-          rows,
-          headers,
-          getHeaderProps,
-          getRowProps,
-          getTableProps,
-          getToolbarProps,
-          getTableContainerProps
-        }) => (
-          <TableContainer
-            title="Controllers"
-            description="A Controller is the thing you use to actually make things happen."
-            {...getTableContainerProps()}
-          >
-            <TableToolbar {...getToolbarProps()} aria-label="data table toolbar">
-              <TableToolbarContent>
-                <TableToolbarSearch onChange={(e) => setFilter(e.target.value)} />
-                <Button renderIcon={Add24} onClick={() => {
-                  history.push({ pathname: `/cores/${contextRealm.coreID}/realms/${contextRealm.id}/config/controllers/new` })
-                }}>New Controller</Button>
-              </TableToolbarContent>
-            </TableToolbar>
-            {rawData.length > 0 &&
-              <>
-                <Table {...getTableProps()}>
-                  <TableHead>
-                    <TableRow>
-                      {headers.map((header, i) => (
-                        <TableHeader key={i} {...getHeaderProps({ header })}>
-                          {header.header}
-                        </TableHeader>
-                      ))}
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {rows.map((row, i) => (
-                      <TableRow key={i} {...getRowProps({ row })}>
-                        {row.cells.map((cell) => (
-                          <TableCell key={cell.id}>{cell.value}</TableCell>
+    if (rawData.length === 0) {
+      return (
+        <Grid className='dx--table-empty'>
+          <Row>
+            <Column sm={{ span: 1 }}>
+              <span style={{ display: 'inline-block', height: '1rem' }}>&nbsp;</span>
+              <Row>
+                <h4>{contextRealm.coreLabel}{contextRealm.id === 'ROOT' ? '' : ` / ${contextRealm.label}`} has no Controllers</h4>
+              </Row>
+              <span style={{ display: 'inline-block', height: '1rem' }}>&nbsp;</span>
+              <Row>
+                A Controller is the thing you use to actually make things happen, it can be a physical controller that connects over the network or a virtual controller that can be used via a web browser.
+              </Row>
+              <span style={{ display: 'inline-block', height: '1rem' }}>&nbsp;</span>
+              <Row>
+                <Button renderIcon={Add24} onClick={() => { history.push({ pathname: `/cores/${contextRealm.coreID}/realms/${contextRealm.id}/config/controllers/new` }) }}>Create A Virtual Controller</Button>
+              </Row>
+            </Column>
+            <Column>
+              <Row>
+                <span style={{ display: 'inline-block', height: '4rem' }}>&nbsp;</span>
+              </Row>
+              <Row>
+                <img src={image} width='60%' style={{ marginLeft: '25%' }}/>
+              </Row>
+            </Column>
+          </Row>
+          <span style={{ display: 'inline-block', height: '4rem' }}>&nbsp;</span>
+        </Grid>
+      )
+    }
+
+    if (rawData.length > 0) {
+      return (
+        <DataTable
+          rows={currentTableData.map(controller => ({ panelLabel: controller.panel?.label, ...controller })) ?? []}
+          headers={headers}
+          render={({
+            rows,
+            headers,
+            getHeaderProps,
+            getRowProps,
+            getTableProps,
+            getToolbarProps,
+            getTableContainerProps
+          }) => (
+            <TableContainer
+              title="Controllers"
+              {...getTableContainerProps()}
+            >
+              <TableToolbar {...getToolbarProps()} aria-label="data table toolbar">
+                <TableToolbarContent>
+                  <TableToolbarSearch onChange={(e) => setFilter(e.target.value)} />
+                  <Button renderIcon={Add24} onClick={() => {
+                    history.push({ pathname: `/cores/${contextRealm.coreID}/realms/${contextRealm.id}/config/controllers/new` })
+                  }}>New Controller</Button>
+                </TableToolbarContent>
+              </TableToolbar>
+              {rawData.length > 0 &&
+                <>
+                  <Table {...getTableProps()}>
+                    <TableHead>
+                      <TableRow>
+                        {headers.map((header, i) => (
+                          <TableHeader key={i} {...getHeaderProps({ header })}>
+                            {header.header}
+                          </TableHeader>
                         ))}
-                        <TableCell>
-                          <ModalStateManager
-                            LauncherContent={({ setOpen }) => (
-                              <OverflowMenu flipped>
-                                <OverflowMenuItem itemText='Edit Controller' onClick={() => {
-                                  history.push({ pathname: `/cores/${contextRealm.coreID}/realms/${contextRealm.id}/config/controllers/${row.cells[0].value}` })
-                                }} />
-                                <OverflowMenuItem itemText='Delete Controller' isDelete disabled={!isDeletable(row.cells)} onClick={() => { setOpen(true) }} />
-                              </OverflowMenu>
-                            )}
-                            ModalContent={({ open, setOpen }) => (
-                              <DeleteObjectModal
-                                open={open}
-                                setOpen={setOpen}
-                                type='device'
-                                id={row.cells[0].value}
-                                label={row.cells[1].value}
-                                deleteFunction={deleteControllerMutation}
-                                refreshFunction={refresh}
-                              />
-                            )} />
-                        </TableCell>
                       </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-                <Pagination
-                  style={{ width: '100%' }}
-                  backwardText="Previous page"
-                  forwardText="Next page"
-                  itemsPerPageText="Items per page:"
-                  page={page}
-                  pageNumberText="Page Number"
-                  pageSize={pageSize}
-                  pageSizes={[10, 25, 50, 100]}
-                  totalItems={filteredTableData.length}
-                  onChange={(e) => {
-                    setPage(e.page)
-                    setPageSize(e.pageSize)
-                  }}
-                />
-              </>
-            }
-          </TableContainer>
-        )}
-      />
-    )
+                    </TableHead>
+                    <TableBody>
+                      {rows.map((row, i) => (
+                        <TableRow key={i} {...getRowProps({ row })}>
+                          {row.cells.map((cell) => (
+                            <TableCell key={cell.id}>{cell.value}</TableCell>
+                          ))}
+                          <TableCell>
+                            <ModalStateManager
+                              LauncherContent={({ setOpen }) => (
+                                <OverflowMenu flipped>
+                                  <OverflowMenuItem itemText='Edit Controller' onClick={() => {
+                                    history.push({ pathname: `/cores/${contextRealm.coreID}/realms/${contextRealm.id}/config/controllers/${row.cells[0].value}` })
+                                  }} />
+                                  <OverflowMenuItem itemText='Delete Controller' isDelete disabled={!isDeletable(row.cells)} onClick={() => { setOpen(true) }} />
+                                </OverflowMenu>
+                              )}
+                              ModalContent={({ open, setOpen }) => (
+                                <DeleteObjectModal
+                                  open={open}
+                                  setOpen={setOpen}
+                                  type='device'
+                                  id={row.cells[0].value}
+                                  label={row.cells[1].value}
+                                  deleteFunction={deleteControllerMutation}
+                                  refreshFunction={refresh}
+                                />
+                              )} />
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                  <Pagination
+                    style={{ width: '100%' }}
+                    backwardText="Previous page"
+                    forwardText="Next page"
+                    itemsPerPageText="Items per page:"
+                    page={page}
+                    pageNumberText="Page Number"
+                    pageSize={pageSize}
+                    pageSizes={[10, 25, 50, 100]}
+                    totalItems={filteredTableData.length}
+                    onChange={(e) => {
+                      setPage(e.page)
+                      setPageSize(e.pageSize)
+                    }}
+                  />
+                </>
+              }
+            </TableContainer>
+          )}
+        />
+      )
+    }
   }
 }
 
--- packages/ui/src/View/Panels/Panels.jsx
+++ packages/ui/src/View/Panels/Panels.jsx
@@ -2,13 +2,15 @@ import React, { useState, useContext } from 'react'
 import { useQuery, useMutation } from 'urql'
 import { useHistory } from 'react-router-dom'
 import { Add24 } from '@carbon/icons-react'
-import { Button, DataTable, DataTableSkeleton, OverflowMenu, OverflowMenuItem, Pagination } from 'carbon-components-react'
+import { Button, Column, DataTable, DataTableSkeleton, Grid, OverflowMenu, OverflowMenuItem, Pagination, Row } from 'carbon-components-react'
 import { panelsGQL, deletePanelGQL } from './queries'
 import ModalStateManager from '../components/ModalStateManager.jsx'
 import DeleteObjectModal from '../components/DeleteObjectModal.jsx'
 import headers from './panelsHeaders'
 import globalContext from '../../globalContext'
 import GraphQLError from '../components/GraphQLError.jsx'
+
+import image from './undraw_control_panel1_20gm.svg'
 
 const { Table, TableContainer, TableHead, TableHeader, TableRow, TableBody, TableCell, TableToolbar, TableToolbarContent, TableToolbarSearch } = DataTable
 
@@ -45,95 +47,129 @@ const Panels = () => {
         .slice(begin, begin + pageSize)
       )[page - 1]
 
-    return (
-      <div>
-        <DataTable
-          rows={currentTableData ?? []}
-          headers={headers}
-          render={({
-            rows,
-            headers,
-            getHeaderProps,
-            getRowProps,
-            getTableProps,
-            getToolbarProps,
-            getTableContainerProps
-          }) => (
-            <TableContainer
-              title="Panels"
-              description="Panels are where you organise and assign Stacks to buttons."
-              {...getTableContainerProps()}
-            >
-              <TableToolbar {...getToolbarProps()} aria-label="data table toolbar">
-                <TableToolbarContent>
-                  <TableToolbarSearch onChange={(e) => setFilter(e.target.value)} />
-                  <Button renderIcon={Add24} onClick={() => { history.push({ pathname: 'panels/new' }) }}>New Panel</Button>
-                </TableToolbarContent>
-              </TableToolbar>
-              { rawData.length > 0 &&
-                <>
-                  <Table {...getTableProps()}>
-                    <TableHead>
-                      <TableRow>
-                        {headers.map((header, i) => (
-                          <TableHeader key={i} {...getHeaderProps({ header })}>
-                            {header.header}
-                          </TableHeader>
-                        ))}
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {rows.map((row, i) => (
-                        <TableRow key={i} {...getRowProps({ row })}>
-                          {row.cells.map((cell) => (
-                            <TableCell key={cell.id}>{cell.value}</TableCell>
+    if (rawData.length === 0) {
+      return (
+        <Grid className='dx--table-empty'>
+          <Row>
+            <Column sm={{ span: 1 }}>
+              <span style={{ display: 'inline-block', height: '1rem' }}>&nbsp;</span>
+              <Row>
+                <h4>{contextRealm.coreLabel}{contextRealm.id === 'ROOT' ? '' : ` / ${contextRealm.label}`} has no Panels</h4>
+              </Row>
+              <span style={{ display: 'inline-block', height: '1rem' }}>&nbsp;</span>
+              <Row>
+                Panels are where you organise and assign Stacks to buttons. You will need to create some to make the best use of Director, but you can use it without them by executing Stacks directly.
+              </Row>
+              <span style={{ display: 'inline-block', height: '1rem' }}>&nbsp;</span>
+              <Row>
+                <Button renderIcon={Add24} onClick={() => { history.push({ pathname: `/cores/${contextRealm.coreID}/realms/${contextRealm.id}/config/panels/new` }) }}>Create A Panel</Button>
+              </Row>
+            </Column>
+            <Column>
+              <Row>
+                <span style={{ display: 'inline-block', height: '4rem' }}>&nbsp;</span>
+              </Row>
+              <Row>
+                <img src={image} width='60%' style={{ marginLeft: '25%' }}/>
+              </Row>
+            </Column>
+          </Row>
+          <span style={{ display: 'inline-block', height: '4rem' }}>&nbsp;</span>
+        </Grid>
+      )
+    }
+
+    if (rawData.length > 0) {
+      return (
+        <div>
+          <DataTable
+            rows={currentTableData ?? []}
+            headers={headers}
+            render={({
+              rows,
+              headers,
+              getHeaderProps,
+              getRowProps,
+              getTableProps,
+              getToolbarProps,
+              getTableContainerProps
+            }) => (
+              <TableContainer
+                title="Panels"
+                description="Panels are where you organise and assign Stacks to buttons."
+                {...getTableContainerProps()}
+              >
+                <TableToolbar {...getToolbarProps()} aria-label="data table toolbar">
+                  <TableToolbarContent>
+                    <TableToolbarSearch onChange={(e) => setFilter(e.target.value)} />
+                    <Button renderIcon={Add24} onClick={() => { history.push({ pathname: 'panels/new' }) }}>New Panel</Button>
+                  </TableToolbarContent>
+                </TableToolbar>
+                { rawData.length > 0 &&
+                  <>
+                    <Table {...getTableProps()}>
+                      <TableHead>
+                        <TableRow>
+                          {headers.map((header, i) => (
+                            <TableHeader key={i} {...getHeaderProps({ header })}>
+                              {header.header}
+                            </TableHeader>
                           ))}
-                          <TableCell>
-                            <ModalStateManager
-                              LauncherContent={({ setOpen }) => (
-                                <OverflowMenu flipped>
-                                  <OverflowMenuItem itemText='Edit Panel' onClick={() => history.push({ pathname: `panels/${row.cells[0].value}` })} />
-                                  <OverflowMenuItem itemText='Delete Panel' isDelete onClick={() => setOpen(true)} />
-                                </OverflowMenu>
-                              )}
-                              ModalContent={({ open, setOpen }) => (
-                                <DeleteObjectModal
-                                  open={open}
-                                  setOpen={setOpen}
-                                  type='panel'
-                                  id={row.cells[0].value}
-                                  label={row.cells[1].value}
-                                  deleteFunction={deletePanelMutation}
-                                  refreshFunction={refresh}
-                                />
-                              )} />
-                          </TableCell>
                         </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                  <Pagination
-                    style={{ width: '100%' }}
-                    backwardText="Previous page"
-                    forwardText="Next page"
-                    itemsPerPageText="Items per page:"
-                    page={page}
-                    pageNumberText="Page Number"
-                    pageSize={pageSize}
-                    pageSizes={[10, 25, 50, 100]}
-                    totalItems={filteredTableData.length}
-                    onChange={(e) => {
-                      setPage(e.page)
-                      setPageSize(e.pageSize)
-                    }}
-                  />
-                </>
-              }
-            </TableContainer>
-          )}
-        />
-      </div>
-    )
+                      </TableHead>
+                      <TableBody>
+                        {rows.map((row, i) => (
+                          <TableRow key={i} {...getRowProps({ row })}>
+                            {row.cells.map((cell) => (
+                              <TableCell key={cell.id}>{cell.value}</TableCell>
+                            ))}
+                            <TableCell>
+                              <ModalStateManager
+                                LauncherContent={({ setOpen }) => (
+                                  <OverflowMenu flipped>
+                                    <OverflowMenuItem itemText='Edit Panel' onClick={() => history.push({ pathname: `panels/${row.cells[0].value}` })} />
+                                    <OverflowMenuItem itemText='Delete Panel' isDelete onClick={() => setOpen(true)} />
+                                  </OverflowMenu>
+                                )}
+                                ModalContent={({ open, setOpen }) => (
+                                  <DeleteObjectModal
+                                    open={open}
+                                    setOpen={setOpen}
+                                    type='panel'
+                                    id={row.cells[0].value}
+                                    label={row.cells[1].value}
+                                    deleteFunction={deletePanelMutation}
+                                    refreshFunction={refresh}
+                                  />
+                                )} />
+                            </TableCell>
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                    <Pagination
+                      style={{ width: '100%' }}
+                      backwardText="Previous page"
+                      forwardText="Next page"
+                      itemsPerPageText="Items per page:"
+                      page={page}
+                      pageNumberText="Page Number"
+                      pageSize={pageSize}
+                      pageSizes={[10, 25, 50, 100]}
+                      totalItems={filteredTableData.length}
+                      onChange={(e) => {
+                        setPage(e.page)
+                        setPageSize(e.pageSize)
+                      }}
+                    />
+                  </>
+                }
+              </TableContainer>
+            )}
+          />
+        </div>
+      )
+    }
   }
 }
 
