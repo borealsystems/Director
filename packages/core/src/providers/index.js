200674e060c0d30511623cd1995db78602879964
Oliver Herrmann <oliver@monoxane.io>
2020-12-03 19:08:29 +1100

    fix(core): T139 SerialPort dependencies

--- packages/core/src/providers/index.js
+++ packages/core/src/providers/index.js
@@ -1,15 +1,19 @@
-//  eslint-disable
 import fs from 'fs'
 import path from 'path'
 import log from '../utils/log.js'
+const SerialPort = require('serialport')
 
 const providers = []
 
 const providerInterfaces = []
 
 const providerInitMethods = []
+
+let isSerial = false
+
 const initProviders = () => {
   return new Promise((resolve, reject) => {
+    SerialPort.list().then(d => isSerial = d.length > 0)
     log('info', 'core/lib/providers', 'Loading Providers')
     fs.readdir(path.resolve(__dirname, './protocolProviders'), (err, files) => {
       if (err) log('error', 'core/lib/providers', err)
@@ -35,8 +39,13 @@ const initProviders = () => {
           directories.forEach(directory => {
             import(`./deviceProviders/${directory}`)
               .then((module) => {
-                providers.push(module.default.providerRegistration)
-                log('info', 'core/lib/providers', `Loaded ${module.default.providerRegistration.id} (${module.default.providerRegistration.label})`)
+                if (module.default.providerRegistration.protocol === 'Serial' && !isSerial) {
+                  log('info', 'core/lib/providers', `Skipping ${module.default.providerRegistration.id} (${module.default.providerRegistration.label}) as this system has no Serial Interfaces`)
+                }
+                else {
+                  providers.push(module.default.providerRegistration)
+                  log('info', 'core/lib/providers', `Loaded ${module.default.providerRegistration.id} (${module.default.providerRegistration.label})`)
+                }
                 counter--
                 if (counter === 0) { 
                   resolve()
