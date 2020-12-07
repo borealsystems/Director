5df5c903a868158a8b7f24b58ba8fcaba959c4ab
Oliver Herrmann <oliver@monoxane.io>
2020-12-08 01:26:39 +1100

    fix(ui): T159 Duplicate cores from realm data

--- packages/ui/src/View/Stacks/StackWrapper.jsx
+++ packages/ui/src/View/Stacks/StackWrapper.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useQuery } from 'urql'
 import { Loading } from 'carbon-components-react'
 import Stack from './Stack.jsx'
+import globalContext from '../../globalContext'
 
 import GraphQLError from '../components/GraphQLError.jsx'
 
 import { existingStackQueryGQL, newStackQueryGQL } from './queries'
 
 const StackWrapper = ({ match: { params: { id } } }) => {
+  const { contextRealm } = useContext(globalContext)
   const [result] = useQuery({
     query: id === 'new' ? newStackQueryGQL : existingStackQueryGQL,
-    variables: id === 'new' ? null : { id: id }
+    variables: id === 'new' ? { realm: contextRealm.id, core: contextRealm.coreID } : { id: id }
   })
 
   if (result.error) return <GraphQLError error={result.error} />
